8d652ce34d06356234c79c4c5b9a30fd23646252
ngambasty-tech <ngambasty@gmail.com>
2026-05-25 13:36:12 +0100

    why not to use the any keyword(any is not a specific type is ts)

--- 01-intro/variables.ts
+++ 01-intro/variables.ts
@@ -14,4 +14,14 @@ console.log(age);
 let isStudent: boolean = true;
 //isStudent = "yes"; // This will cause a type error because 'isStudent' is declared as a boolean
 console.log(isStudent); 
+
+//let meal; // TypeScript infers the type of 'meal' as 'any' because it is declared without an initial value. The 'any' type allows 'meal' to hold any type of value, which can lead to potential issues if not used carefully.
+let meal: string; // By explicitly declaring 'meal' as a string, we can ensure that it can only hold string values, which helps prevent type-related errors and improves code safety.
+
+function orderMeal() {
+    return "Pizza"; //now if i try to return a number or boolean, i automatically get an error because the return type of the function is inferred as string based on the returned value. This helps catch potential issues early and ensures that the function's return type is consistent with the expected type of 'meal'.
+}
+meal = orderMeal(); // 'meal' can be assigned a string value returned by the 'orderMeal' function without any type errors because it is of type 'any'.
+// the function ordermeal returns a string "pizza" but nothing stops it from returning a boolean or a number because 'meal' is of type 'any'. This can lead to potential issues if the function's return type is not consistent with the expected type of 'meal' which is a string.
+
 export {}; // This line is added to make this file a module, preventing global scope pollution.
