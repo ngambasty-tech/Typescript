ae111306d7a9b7cdf13fcef4fd46a675da80bb27
ngambasty-tech <ngambasty@gmail.com>
2026-05-27 11:03:32 +0100

    type aliases

--- 02-functions/myobjects.ts
+++ 02-functions/myobjects.ts
@@ -1,17 +1,26 @@
-const User = {
-    name: "Alice",
-    email: "alice@example.com",
-    isActive: true
+// const User = {
+//     name: "Alice",
+//     email: "alice@example.com",
+//     isActive: true
+// }
+
+// function createUser({name:string, isActive:boolean}) {
+// }
+
+// // createUser({name: "Bob", isActive: true})
+// let newUser = {name: "Bob", isActive: true, email: "bob@email.com"}
+
+// function createCourse(): {name: string, price: number} {
+//     return {name: "js", price: 3000}
+// }
+
+type user ={
+    name: string,
+    email: string,
+    isPaid: boolean
+}//when ever there are methods in the object we use type instead of interface
+
+function createUser(user: user):user {
+    return{name: user.name, email: user.email, isPaid: user.isPaid }
 }
-
-function createUser({name:string, isActive:boolean}) {
-}
-
-// createUser({name: "Bob", isActive: true})
-let newUser = {name: "Bob", isActive: true, email: "bob@email.com"}
-
-function createCourse(): {name: string, price: number} {
-    return {name: "js", price: 3000}
-}
-
 export {}
