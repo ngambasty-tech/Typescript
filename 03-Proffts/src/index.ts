7992e6edec707111be6bc576bdaec69d88461b3e
ngambasty-tech <ngambasty@gmail.com>
2026-06-01 23:19:02 +0100

    classes

--- 03-Proffts/src/index.ts
+++ 03-Proffts/src/index.ts
@@ -1,6 +1,6 @@
 class User {
             email: string
-        name: string
+        name: string// readonly properties can only be assigned once, either at the time of declaration or within the constructor
         readonly city: string = ""
     constructor(email: string, name: string) {
         this.email = email;
@@ -8,5 +8,5 @@ class User {
     }
 }
 
-const Seb = new User("seb@example.com", "Sebastian");
+const Seb = new User("seb@example.com", "Sebastian");//thid is an instance of the User class
 // Seb.city = "Bamenda";
