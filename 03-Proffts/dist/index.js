a43798ed0a9564a605dcc1c6ad490fa6be12559c
ngambasty-tech <ngambasty@gmail.com>
2026-06-03 21:56:03 +0100

    getters and setters

--- 03-Proffts/dist/index.js
+++ 03-Proffts/dist/index.js
@@ -13,10 +13,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class User {
     email;
     name;
+    _courseCount = 1; //this is a private property that can only be accessed within the class. It is initialized with a default value of 1.
     city = "Kom";
     constructor(email, name) {
         this.email = email;
         this.name = name;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    //get is used to get any property, and set to set any property, nott necessaily private properties.
+    //these getters and setters anre designed so that these private properties can be exposed outside with additional logic or restrictions.
+    //and the have no return type, because they are not regular methods, they are accessed like properties, so we dont need to specify a return type for them.
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`; //this is a getter method that returns a string that combines the word "apple" with the user's email. It allows us to access this computed value as if it were a property, without needing to call it like a regular method. For example, we can access it using `Seb.getAppleEmail` instead of `Seb.getAppleEmail()`.
     }
 }
 const Seb = new User("seb@example.com", "Sebastian"); //thid is an instance of the User class
--- 03-Proffts/src/index.ts
+++ 03-Proffts/src/index.ts
@@ -10,6 +10,9 @@
 //}
 
 class User {
+
+    private _courseCount = 1;//this is a private property that can only be accessed within the class. It is initialized with a default value of 1.
+
     readonly city: string = "Kom";
     constructor(
         public email: string,
@@ -17,6 +20,27 @@ class User {
         // private readonly userId: string,
     ) {//better way of declaring and initializing properties in the constructor is to use parameter properties, which allows us to declare and initialize properties directly in the constructor parameters, reducing boilerplate code
 
+    }
+    private deleteToken(){
+        console.log("Token deleted");
+    }
+
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+//get is used to get any property, and set to set any property, nott necessaily private properties.
+//these getters and setters anre designed so that these private properties can be exposed outside with additional logic or restrictions.
+//and the have no return type, because they are not regular methods, they are accessed like properties, so we dont need to specify a return type for them.
+    set courseCount(courseNum) {
+        if (courseNum <= 1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum;
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`; //this is a getter method that returns a string that combines the word "apple" with the user's email. It allows us to access this computed value as if it were a property, without needing to call it like a regular method. For example, we can access it using `Seb.getAppleEmail` instead of `Seb.getAppleEmail()`.
     }
 }
 
