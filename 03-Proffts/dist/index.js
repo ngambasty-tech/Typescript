f3f8c668b4c7722976872872967e3c9f84d23fca
ngambasty-tech <ngambasty@gmail.com>
2026-06-03 21:36:34 +0100

    private/public

--- 03-Proffts/dist/index.js
+++ 03-Proffts/dist/index.js
@@ -1,14 +1,26 @@
 "use strict";
+//class User {
+//       public email: string
+//     name: string// readonly properties can only be assigned once, either at the time of declaration or within the constructor
+//   private readonly city: string = ""
+//constructor(email: string, name: string) {
+//  this.email = email;
+//this.name = name;
+//this.city // i can use it here as it is within the constructor, but i cannot assign it a value here because it is already initialized with an empty string
+//}
+//}
 Object.defineProperty(exports, "__esModule", { value: true });
 class User {
     email;
     name;
-    city = "";
+    city = "Kom";
     constructor(email, name) {
         this.email = email;
         this.name = name;
     }
 }
-const Seb = new User("seb@example.com", "Sebastian");
-// Seb.city = "Bamenda";
+const Seb = new User("seb@example.com", "Sebastian"); //thid is an instance of the User class
+// Seb.city = "Bamenda";//i cannot asign a readonly property after it has been initialized
+//Seb.city //with the readonly i can just read it
+//Seb.city //now i cant access the city property because it is private, it can only be accessed within the class
 //# sourceMappingURL=index.js.map
--- 03-Proffts/src/index.ts
+++ 03-Proffts/src/index.ts
@@ -1,12 +1,28 @@
+//class User {
+ //       public email: string
+   //     name: string// readonly properties can only be assigned once, either at the time of declaration or within the constructor
+     //   private readonly city: string = ""
+    //constructor(email: string, name: string) {
+      //  this.email = email;
+        //this.name = name;
+        //this.city // i can use it here as it is within the constructor, but i cannot assign it a value here because it is already initialized with an empty string
+    //}
+//}
+
 class User {
-            email: string
-        name: string// readonly properties can only be assigned once, either at the time of declaration or within the constructor
-        readonly city: string = ""
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+    readonly city: string = "Kom";
+    constructor(
+        public email: string,
+        public name: string,
+        // private readonly userId: string,
+    ) {//better way of declaring and initializing properties in the constructor is to use parameter properties, which allows us to declare and initialize properties directly in the constructor parameters, reducing boilerplate code
+
     }
 }
 
 const Seb = new User("seb@example.com", "Sebastian");//thid is an instance of the User class
-// Seb.city = "Bamenda";
+// Seb.city = "Bamenda";//i cannot asign a readonly property after it has been initialized
+//Seb.city //with the readonly i can just read it
+//Seb.city //now i cant access the city property because it is private, it can only be accessed within the class
+
+
